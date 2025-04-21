9979a0ebb60f7c3913c0590766431562b8c3dc35
Dn-iel <danielsnow229@gmail.com>
2025-04-22 00:08:05 +0700

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -28,7 +28,7 @@ def main():
     person_income = st.number_input("Income", min_value=0.0, value=5000.0)
     person_emp_exp = st.number_input("Years of Work Expreience", min_value=0, value=3)
     person_home_ownership = st.selectbox("Home Ownership Status", ["MORTGAGE", "OWN", "RENT", "OTHER"])
-    loan_amnt = st.number_input("Loan Amount", min_value=1000.0, value=10000.0)
+    loan_amnt = st.number_input("Loan Amount", min_value=1000.0, value=1000.0)
     loan_intent = st.selectbox("Loan Intent", ["DEBTCONSOLIDATION", "EDUCATION", "HOMEIMPROVEMENT", "MEDICAL", "PERSONAL", "VENTURE" ])
     loan_int_rate = st.number_input("Loan Interest (%)", min_value=0.0, value=10.0)
     
